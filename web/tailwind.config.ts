19462a94a87e5893b5fada2626a879996d3502b7
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2026-08-04 18:15:10 +0100

    Updated Tailwind CSS `content` paths

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-darkMode: 'class',
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
